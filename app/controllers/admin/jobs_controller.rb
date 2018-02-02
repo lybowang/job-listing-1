c9ff6e5899c863fad84b0c6cbfac501e10dc0f6f
lybowang <lybowang@gmail.com>
2018-02-02 23:10:44 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,51 +1,61 @@
 class Admin::JobsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-	before_action :require_is_admin
+    before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :require_is_admin
 
-	def show
-      @job = Job.find(params[:id])
-	end
-	
-	def index
-	  @jobs = Job.all
-	end
-	
-	def new
-	  @job = Job.new
-	end
-	
-	def create
-	  @job = Job.new(job_params)  
+  def show
+    @job = Job.find(params[:id])
+  end
 
-      if @job.save
-      	redirect_to admin_jobs_path
-      else
-        render :new
-      end
-    end
+  def index
+    @jobs = Job.all
+  end
 
-    def edit
-      @job = Job.find(params[:job])    	
-      if @job.update(job_params)
-      	 redirect_to admin_jobs_path
-      else
-        render :edit
-      end
-    end
+  def new
+    @job = Job.new
+  end
 
-    def destroy
-      @job = Job.find(params[:id])    	 
+  def create
+    @job = Job.new(job_params)
 
-      @job.destroy
-
+    if @job.save
       redirect_to admin_jobs_path
+    else
+      render :new
     end
-    
+  end
 
-    private
+  def edit
+    @job = Job.find(params[:id])
+  end
 
-    def job_params
-      params.require(:job).permit(:title, :description)
-    end  
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+
+    redirect_to admin_jobs_path
+  end
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end
 end
-
